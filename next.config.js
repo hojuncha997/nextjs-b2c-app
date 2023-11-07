d28dc1052b3c4837740a781b04efe615994e021c
unknown <hojun.cha997@gmail.com>
2023-11-07 23:28:02 +0900

    next.js 프로젝트 생성

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    // styledComponents 활성화
+    styledComponents: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
