8c95a0e01533fed779ed24cae5ebfc36c9fb53b3
unknown <hojun.cha997@gmail.com>
2023-11-08 17:50:05 +0900

    프로젝트 환경설정 완료

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
+  compiler: (() => {
     // styledComponents 활성화
-    styledComponents: true,
-  },
+    let compilerConfig = { styledComponents: true };
+
+    if (process.env.NODE_ENV === "production") {
+      compilerConfig = {
+        ...(compilerConfig = {
+          // 프로덕션 환경에서는 리액트 테스팅 라이브러리에서 사용하는 data-testid 속성 삭제
+          reactRemoveProperties: { properties: ["^data-testid$"] },
+        }),
+      };
+      return compilerConfig;
+    }
+  })(),
 };
 
 module.exports = nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import { AppProps } from "next/app";
+import { AppProps } from 'next/app'
 import Head from 'next/head'
-import {createGlobalStyle} from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
 
 // 글로벌 스타일
 const GlobalStyle = createGlobalStyle`
@@ -29,21 +29,22 @@ const GlobalStyle = createGlobalStyle`
 
 `
 
-const MyApp = ({Component, pageProps}: AppProps) => {
-    return (
-        <>
-            <Head>
-                <meta key="charset" name="charset" content="utf-8"/>
-                <meta key="viewport" 
-                name="viewport"
-                content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
-                />
-                <meta property="og:locale" content="ko_KR"/>
-                <meta property="og:type" content="website"/>
-            </Head>
-            <GlobalStyle/>
-            <Component {...pageProps} />
-        </>
-    )
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <meta key="charset" name="charset" content="utf-8" />
+        <meta
+          key="viewport"
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
+        />
+        <meta property="og:locale" content="ko_KR" />
+        <meta property="og:type" content="website" />
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
 export default MyApp
