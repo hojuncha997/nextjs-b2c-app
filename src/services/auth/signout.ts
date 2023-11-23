b160027ed2328ffc41784a73f0fbc629867a001d
Hojun Cha <hojun.cha997@gmail.com>
2023-11-23 21:53:42 +0900

    signout

---
+++ src/services/auth/signout.ts
@@ -0,0 +1,21 @@
+import type { ApiContext } from "@/types/data";
+import { fetcher } from "@/utils";
+
+/**
+ * 인증 API(로그아웃)
+ * @param context API 컨텍스트
+ * @returns 로그아웃 메시지
+ */
+
+const signout = async (context : ApiContext): Promise<{message: string}> => {
+    return await fetcher(
+        `${context.apiRootUrl.replace(/\/$/g, '')}/auth/signout`,
+        {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            }
+        }
+    )
+}
